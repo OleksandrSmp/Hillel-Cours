ad0c063062cda29e39e0648273b741cc39b713aa
Aleksandr <aleksandrklysa7@gmail.com>
2024-01-23 20:46:01 +0200

    lesson2

---
+++ lesson2/dz_2.1.py
@@ -0,0 +1,38 @@
+user = int(input('Введите четырехзначное число:'))
+a = 1000
+b = 100
+c = 10
+
+num1 = user // a
+num2 = user % a
+
+num3 = num2 // b
+num4 = num2 % b
+
+num5 = num4 // c
+num6 = num4 % c
+
+print(num1)
+print(num3)
+print(num5)
+print(num6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
